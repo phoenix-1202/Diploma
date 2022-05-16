c714184a9db27014224d789d6e110e74aa71aecb
Olesya Chebotareva <71000623+phoenix-1202@users.noreply.github.com>
2022-05-17 02:00:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from model import LayoutGAN
 
 def main():
     layout_gan = LayoutGAN()
-    layout_gan.train()
+    # layout_gan.train()
+    layout_gan.test("./checkpoint/state_064.pth")
 
 
 if __name__ == '__main__':
